bee95f86c336f62293e7a7aa9b28067bbe9cfcd1
Kyra <klz245@nyu.edu>
2024-12-07 11:57:12 -0800

    needle reflection off enemy - V.2, reflects along curvature of a sphere to encourage deflection away from player

--- Assets/Enemy/Prototypes/EnemyPrototype_V1.cs
+++ Assets/Enemy/Prototypes/EnemyPrototype_V1.cs
@@ -9,13 +9,23 @@ public class EnemyPrototype_V1 : MonoBehaviour
     public static float CurrHealth { get; private set; }
     public static float MaxHealth { get; private set; }
 
+    private bool acceptCollision = true;
+    private float timeOfLastCollision;
+    private const float COLLISION_BUFFER_DURATION = 1f;
+
     private void Awake() {
         CurrHealth = maxHealth;
         MaxHealth = maxHealth;
     }
 
+    private void Update() {
+        //check if it's been long enough from the last collision to accept collisions again
+        if (!acceptCollision)
+            acceptCollision = Time.time - timeOfLastCollision >= COLLISION_BUFFER_DURATION;
+    }
+
     private void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject.CompareTag("Needle")) {
+        if (col.gameObject.CompareTag("Needle") && acceptCollision) {
             NeedleScript needle = col.GetComponent<NeedleScript>();
 
             //safety check: has NeedleScript component at root
@@ -26,6 +36,8 @@ public class EnemyPrototype_V1 : MonoBehaviour
 
             TakeDamage(needle.Damage);
             HandleReflection(needle);
+
+            acceptCollision = false;
         }
     }
 
@@ -35,9 +47,11 @@ public class EnemyPrototype_V1 : MonoBehaviour
     }
 
     private void HandleReflection(NeedleScript needle) {
-        Vector2 dir = (needle.transform.position - transform.position).normalized;
+        Vector2 normal = needle.transform.position - transform.position;
+        Vector2 reflectedDir = Vector2.Reflect(needle.Velocity, normal).normalized;
+
         float speed = needle.Velocity.magnitude;
-        needle.SetVelocity(dir * speed);
+        needle.SetVelocity(reflectedDir * speed);
     }
 
 }
